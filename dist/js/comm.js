14c979a9811fec90bd697ac42180c7ce9a3659dc
choi11223344 <choi890220@gmail.com>
2023-09-04 18:17:06 +0900

    230904-slider

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -11,3 +11,35 @@ var swiper = new Swiper(".slide-intro", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// var swiper = new Swiper(".mySwiper", {
+//   loop: true,
+//   spaceBetween: 10,
+//   slidesPerView: 1,
+//   // slidesPerGroup: 1,
+//   freeMode: true,
+//   watchSlidesProgress: true,
+// });
+// var swiper2 = new Swiper(".mySwiper2", {
+//   loop: true,
+//   spaceBetween: 20,
+//   slidesPerView: 3,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: swiper,
+//   },
+// });
+
+// var swiper2 = new Swiper2(".mySwiper2", {
+//   direction: "vertical",
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   mousewheel: true,
+//   pagination: {
+//     el: ".swiper-pagination2",
+//     clickable: true,
+//   },
+// });
