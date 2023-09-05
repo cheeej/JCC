efef29da67aba05c695c9982d872c6685bea4c41
choi11223344 <136055434+choi11223344@users.noreply.github.com>
2023-09-06 00:04:18 +0900

    230906-goods

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,25 +1,8 @@
-
-
-  $(function(){
-    $(".Accordion > ul > li").mouseover(function(){
-      $(".Accordion > ul > li > ul").stop().slideDown();
-    })
+$(function () {
+  $(".depth1").mouseover(function () {
+    $(this).find(".depth2").stop().slideDown();
   });
-
-// //Slick - Carousel Double
-// $(document).ready(function () {
-//   //Text Portion
-//   var $status = $(".pagingInfo");
-//   var $slickElement = $(".carousel-double--txt");
-
-//   $slickElement.on(
-//     "init reInit afterChange",
-//     function (event, slick, currentSlide, nextSlide) {
-//       var i = (currentSlide ? currentSlide : 0) + 1;
-//       $status.text(i + "/" + slick.slideCount);
-//     }
-//   );
-
-// });
-
-
+  $(".depth1").mouseout(function () {
+    $(this).find(".depth2").stop().slideUp();
+  });
+});
