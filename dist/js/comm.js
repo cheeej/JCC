6682d76ef34acdff1f4ba53122d349fefcaa5f96
choi11223344 <choi890220@gmail.com>
2023-09-06 18:16:25 +0900

    230906 -goods

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,53 +1,12 @@
-// var swiper = new Swiper(".slide-intro", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
-//Slick - Carousel Double
-$(document).ready(function () {
-  //Text Portion
-  var $status = $(".pagingInfo");
-  var $slickElement = $(".carousel-double--txt");
-
-  $slickElement.on(
-    "init reInit afterChange",
-    function (event, slick, currentSlide, nextSlide) {
-      var i = (currentSlide ? currentSlide : 0) + 1;
-      $status.text(i + "/" + slick.slideCount);
-    }
-  );
-
-  //Text Portion - Options
-  $slickElement.slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    asNavFor: ".carousel-double--img",
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    pauseOnDotsHover: false,
-  });
-
-  //Image Portion
-  $(".carousel-double--img").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    asNavFor: ".carousel-double--txt",
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: false,
-    pauseOnDotsHover: false,
-    nextArrow: ".carousel-double--next",
-    prevArrow: ".carousel-double--prev",
-  });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
 });
