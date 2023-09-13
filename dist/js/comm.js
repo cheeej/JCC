50a5340243c53ea138854e1c4b7ddf5e3f08bfe3
choi11223344 <choi890220@gmail.com>
2023-09-13 18:15:52 +0900

    230913-수정중,,,,,

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -4,7 +4,40 @@ $(function () {
   });
 });
 
-var swiper = new Swiper(".mySwiper", {
+$(function () {
+  $(".carousel-item").eq(0).addClass("active");
+  var total = $(".carousel-item").length;
+  var current = 0;
+  $("#moveRight").on("click", function () {
+    var next = current;
+    current = current + 1;
+    setSlide(next, current);
+  });
+  $("#moveLeft").on("click", function () {
+    var prev = current;
+    current = current - 1;
+    setSlide(prev, current);
+  });
+  function setSlide(prev, next) {
+    var slide = current;
+    if (next > total - 1) {
+      slide = 0;
+      current = 0;
+    }
+    if (next < 0) {
+      slide = total - 1;
+      current = total - 1;
+    }
+    $(".carousel-item").eq(prev).removeClass("active");
+    $(".carousel-item").eq(slide).addClass("active");
+    setTimeout(function () {}, 800);
+
+    console.log("current " + current);
+    console.log("prev " + prev);
+  }
+});
+
+var swiper = new Swiper(".slide-intro", {
   slidesPerView: 2.5,
   spaceBetween: 30,
   pagination: {
@@ -14,5 +47,14 @@ var swiper = new Swiper(".mySwiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
     hide: true,
+  },
+});
+
+var swiper = new Swiper(".slide-goods", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
